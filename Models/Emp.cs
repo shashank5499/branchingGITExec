17385a9635cc3722b442d05cc9dfeb4ba2436ab8
Shashank Thakur <shashank.thakur.1848816@gmail.com>
2020-12-03 16:25:10 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppBranchingExercise.Models
+{
+    [Table("Employees")]
+    public class Emp
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
